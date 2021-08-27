480327f006aae6f6b65a47e5c7a73f8bf79eee1f
Ferney Mejia <80012964+FerneyMP@users.noreply.github.com>
2021-08-26 19:30:28 -0500

    Add files via upload

---
+++ Pract4/red.h
@@ -0,0 +1,17 @@
+#ifndef RED_H
+#define RED_H
+#include "enrutador.h"
+
+class red
+{
+public:
+
+    red();
+private:
+    //leer archivo de texto, crear nuevo nodo(enrutador) dentro del mapa red, se crea un nueva clave, si ya existe modificar, si no
+    // agregar. Dato numérico y la segunda letra, agregar en sentido opuesto
+
+
+};
+
+#endif // RED_H
